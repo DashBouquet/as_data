1b4258b8f2dba40c3773e18a1b24ea2402ee650d
Artem Bushuev <bushuev.artem98@gmail.com>
2019-02-28 14:55:00 +0300

    FEATURE changed bigquery/index.js
    added toFormat function
    added getRecordById request
    changed insertIntoTable logic

--- bigquery/index.js
+++ bigquery/index.js
@@ -1,12 +1,26 @@
 const {BigQuery} = require('@google-cloud/bigquery')
+const { format } = require('date-fns')
 
 const logger = require('../logger')
-const { GOOGLE_APPLICATION_CREDENTIALS, PROJECT_ID, FB_DATASET_ID } = require('../.env')
+const { GOOGLE_APPLICATION_CREDENTIALS, PROJECT_ID, FB_DATASET_ID, big_query_await_time_ms } = require('../.env')
 
 const bigquery = new BigQuery({
   keyFilename: GOOGLE_APPLICATION_CREDENTIALS,
   projectId: PROJECT_ID,
 });
+
+const toFormat = (value, type) => {
+  switch (type.toLowerCase()) {
+    case 'string':
+      return `"${value}"`
+    case 'boolean':
+      return value
+    case 'datetime':
+      return `"${format(value, 'YYYY-MM-DD')}"`
+    default:
+      return `"${value}"`
+  }
+}
 
 async function createTables() {
   const adaccount_schema = require('./schema/facebook-adaccount')
@@ -15,11 +29,11 @@ async function createTables() {
   const campaign_schema = require('./schema/facebook-campaign')
   const insight_schema = require('./schema/facebook-insight')
 
-  const [table1] = await bigquery.dataset(FB_DATASET_ID).createTable(`adaccount_${(Math.random()*1000).toFixed()}`, {schema: adaccount_schema})
-  const [table2] = await bigquery.dataset(FB_DATASET_ID).createTable(`ad_${(Math.random()*1000).toFixed()}`, {schema: ad_schema})
-  const [table3] = await bigquery.dataset(FB_DATASET_ID).createTable(`adset_${(Math.random()*1000).toFixed()}`, {schema: adset_schema})
-  const [table4] = await bigquery.dataset(FB_DATASET_ID).createTable(`campaign_${(Math.random()*1000).toFixed()}`, {schema: campaign_schema})
-  const [table5] = await bigquery.dataset(FB_DATASET_ID).createTable(`insight_${(Math.random()*1000).toFixed()}`, {schema: insight_schema})
+  const [table1] = await bigquery.dataset(FB_DATASET_ID).createTable(`adaccount`, {schema: adaccount_schema})
+  const [table2] = await bigquery.dataset(FB_DATASET_ID).createTable(`ad`, {schema: ad_schema})
+  const [table3] = await bigquery.dataset(FB_DATASET_ID).createTable(`adset`, {schema: adset_schema})
+  const [table4] = await bigquery.dataset(FB_DATASET_ID).createTable(`campaign`, {schema: campaign_schema})
+  const [table5] = await bigquery.dataset(FB_DATASET_ID).createTable(`insight`, {schema: insight_schema})
   console.log(`
     Table ${table1.id} created.
     Table ${table2.id} created.
@@ -29,24 +43,52 @@ async function createTables() {
   `)
 }
 
-async function insertIntoTable (table, object) {
+async function getRecordById (table, recordId) {
+  const query = `SELECT * FROM ${table} WHERE id = "${recordId}";`
+  try {
+    const [rows] = await bigquery.query({ query, location: 'US' });
+    return rows
+  }
+  catch (e) {
+    if (e.message.includes('Exceeded rate limits')) {
+      return new Promise(reslove => setTimeout(() => reslove(getRecordById(table, recordId), big_query_await_time_ms)))
+    } else { throw e }
+  }
+}
+
+async function insertIntoTable (table, object, schema) {
   if (!table) { return }
   if (!object) { return }
-  
+
   const keys = Object.keys(object)
+
+  if (keys.length === 2) { return } // the case when only insights.descriptor & insights.id comes
+
+  const isAlreadyExist = (await getRecordById(table, object.id)).length > 0
+
+  if (isAlreadyExist) {
+    console.log('already exist', object.id)
+    return
+  }
+
   const query = `
     INSERT INTO ${table} (${keys.join(',')})
-      VALUES (${keys.map(i => `"${object[i]}"`).join(',')});
+      VALUES (${keys.map(i => `${toFormat(object[i], schema[i].type)}`).join(',')});
   `
   try {
     await bigquery.query({ query });
   } catch(e) {
-    logger.warn('The request will be repeated', {table, object}, e.message)
-    return new Promise((reslove) => { setTimeout(() => { reslove(insertIntoTable(table, object)) }, 1000 * 6) })
+    if (e.message.includes('Exceeded rate limits')) {
+      logger.warn('The request will be repeated', {table, object}, e.message)
+      return new Promise((reslove) => { setTimeout(() => { reslove(insertIntoTable(table, object, schema)) }, big_query_await_time_ms) })
+    } else {
+      throw e
+    }
   }
 }
 
 module.exports = {
   createTables,
   insertIntoTable,
+  getRecordById
 }
